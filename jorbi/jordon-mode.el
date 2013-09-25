196d440997e266e8143305b50232bd7ce3d39310
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-24 23:50:47 -0400

    window keys or terminal

--- jorbi/jordon-mode.el
+++ jorbi/jordon-mode.el
@@ -4,15 +4,14 @@
 (defdev "jordon"
   (set-default-font "-apple-Inconsolata-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
   (define-keys jordon-dev-mode-map
-    ("C-," 'previous-multiframe-window)
     ("C-\\" 'jorbi/toggle-comment-line)
     ("C-x l" 'ibuffer)
     ("C-<tab>" 'jorbi/indent-repeat)
     ("C-c f u" 'winstack-push)
     ("C-c f o" 'winstack-pop)
     ("C-M-k" 'jorbi/c-doc-comment)
-    ("C-." 'next-multiframe-window)))
-
+    ((if window-system "C-," "C-c i") 'previous-multiframe-window)
+    ((if window-system "C-." "C-c o") 'next-multiframe-window)))
 
 
 (provide 'jordon-mode)
