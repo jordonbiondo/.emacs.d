b5d0c4d98b6e40a59a54906322aec7c812e13397
Jordon Biondo <jordon@Jordons-MacBook-Pro.local>
2014-06-10 11:44:58 -0400

    keys.el

---
+++ keys/keys.el
@@ -0,0 +1,10 @@
+(defmacro key (key)
+  (let* ((name (substring (symbol-name key) 1))
+         (file (concat "~/.emacs.d/keys/" name)))
+    (if (file-exists-p file)
+        (with-temp-buffer
+          (insert-file-contents file)
+          (buffer-string))
+      (error "No key found for %S" key))))
+
+(provide 'keys)
