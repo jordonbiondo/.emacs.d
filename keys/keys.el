982e1c1ca0fa741fd5aa05d1b7662f7c22686f8b
jordonbiondo <biondoj@mail.gvsu.edu>
2014-07-21 17:23:33 -0400

    allow no errors on keys

--- keys/keys.el
+++ keys/keys.el
@@ -1,10 +1,11 @@
-(defun key (key)
+(defun key (key &optional no-error)
   (let* ((name (substring (symbol-name key) 1))
          (file (concat "~/.emacs.d/keys/" name)))
     (if (file-exists-p file)
         (with-temp-buffer
           (insert-file-contents file)
           (buffer-string))
-      (error "No key found for %S" key))))
+      (and (not no-error) (error "No key found for %S" key)))))
 
 (provide 'keys)
+
