10012c9ce67c4025b3c00c024f34b27378c719db
jordonbiondo <biondoj@mail.gvsu.edu>
2015-05-19 09:12:44 -0400

    improved theme config

--- init.el
+++ init.el
@@ -345,7 +345,10 @@
                        key-chord-one-key-delay .020)))
 
 (use-package ample-theme
-  :idle (load-theme 'ample-flat t)
+  :init (progn (load-theme 'ample t t)
+               (load-theme 'ample-flat t t)
+               (load-theme 'ample-light t t)
+               (enable-theme 'ample-flat))
   :defer t
   :ensure t)
 
