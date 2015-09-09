564416a60eed4c68a4f8cb892ca1bcac1618edb9
jordonbiondo <jordonbiondo@gmail.com>
2015-09-09 08:52:44 -0400

    defer extra js

--- init.el
+++ init.el
@@ -136,7 +136,8 @@
                 ("C-c n f m" . awt-find-model)
                 ("C-c n r m" . awt-require-model)
                 ("C-c n f c" . awt-find-controller)
-                ("C-c n t" . awt-run-test-file)))))))
+                ("C-c n t" . awt-run-test-file)))
+      :defer t))))
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; built-ins
