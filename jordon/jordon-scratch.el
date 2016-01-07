08c90bf842bd04202cd0ab7ad9485a12a50644de
jordonbiondo <jordonbiondo@gmail.com>
2016-01-07 09:30:24 -0500

    handful of functions for later

--- jordon/jordon-scratch.el
+++ jordon/jordon-scratch.el
@@ -82,17 +82,14 @@
               (lambda(a b) (= 0 (random 2))))))
      (point))))
 
-(font-lock-add-keywords
- 'c-mode
- '(("\\(\\<[A-Z_][A-Z_0-9]+\\)\\( \\)?\\((\\)"
-    1 'font-lock-constant-face)
-   ("\\(#define\\)\\( +\\)\\([A-Z_][A-Za-z0-9_]+\\)"
-    3 'font-lock-constant-face t)
-   ("\\(?:ex\\(?:ec\\(?:l[ep]\\|v[Pep]\\|[lv]\\)\\|it\\)\\|fork\\|wait\\(?:pid\\)?\\)\\( *\\)\\((\\)"
-    1 'font-lock-builtin-face t)))
-
-
-
+;; (font-lock-add-keywords
+;;  'c-mode
+;;  '(("\\(\\<[A-Z_][A-Z_0-9]+\\)\\( \\)?\\((\\)"
+;;     1 'font-lock-constant-face)
+;;    ("\\(#define\\)\\( +\\)\\([A-Z_][A-Za-z0-9_]+\\)"
+;;     3 'font-lock-constant-face t)
+;;    ("\\(?:ex\\(?:ec\\(?:l[ep]\\|v[Pep]\\|[lv]\\)\\|it\\)\\|fork\\|wait\\(?:pid\\)?\\)\\( *\\)\\((\\)"
+;;     1 'font-lock-builtin-face t)))
 
 (defun flycheck-clang-toggle-define-at-point ()
   (interactive)
@@ -107,12 +104,66 @@
               (progn (message "Added definition: %s" pp-def)
                      (cl-adjoin pp-def flycheck-clang-definitions)))))))
 
-
-
 (defmacro toggle-member (element list)
   `(setq ,list (if (member ,element ,list)
                    (cl-remove-if (lambda(s) (equal s ,element)) ,list)
                  (cl-adjoin ,element ,list))))
 
+(defun e$ (type &rest others)
+  "make html"
+  (declare (indent defun))
+  (let* ((given-type (or (and (listp type) (car type)) type))
+         (elem-type (if (keywordp given-type)
+                        (substring (symbol-name given-type) 1)
+                      given-type))
+         (attributes (and (listp type) (cadr type))))
+    (format "<%s%s>%s</%s>"
+            elem-type
+            (or
+             (and attributes
+                  (concat
+                   " "
+                   (mapconcat
+                    (lambda (attr)  (format "%s=\"%s\"" (car attr) (cdr attr)))
+                    attributes
+                    " ")))
+             "")
+            (if others (concat "\n" (string-join others "\n") "\n") "")
+            elem-type)))
 
+(defun render-html-async (file)
+  (promise-async*
+   (shell-command-to-string (format "w3m -dump -cols 80 %s" file))))
 
+(defun jordon-substitute-prefix (name sub form)
+  (let ((prefix (concat name ".")))
+    (if (listp form)
+        (mapcar (lambda (ff) (jordon-substitute-prefix name sub ff)) form)
+      (if (and (symbolp form) (string-prefix-p prefix (symbol-name form)))
+          (prog1 (intern (format "%s%s" sub (substring (symbol-name form) (length prefix))))
+            (message "%S" form))
+        form))))
+
+(defmacro with-prefixes (names &rest body)
+  (declare (indent 1))
+  (let ((body (cons 'progn body)))
+    (dolist (substitution names)
+      (destructuring-bind (prefix sub) substitution
+        (setq body (jordon-substitute-prefix (symbol-name prefix) sub body))))
+    body))
+
+(defun one-off-hook (hook func &optional name)
+  "Add FUNC to HOOK such that the func removes itself when called.
+The symbol created and added to HOOK will optionally end with NAME if
+provided for debugging purposes."
+  (unless lexical-binding (user-error "Using one-off-hook requires `lexical-binding'."))
+  (let* ((hook-name (format "*one-off-hook%s*" (if name (concat "-" name) "")))
+         (hook-sym (make-symbol hook-name)))
+    (setf (symbol-function hook-sym)
+          (lambda (&rest args)
+            (unwind-protect
+                (apply func args)
+              (let ((inhibit-message t))
+                (remove-hook hook hook-sym)
+                (message "removed %s from %s" hook-name hook)))))
+    (add-hook hook hook-sym)))
